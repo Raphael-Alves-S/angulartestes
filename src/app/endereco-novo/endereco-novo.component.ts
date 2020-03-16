439e4b66cdfb12dfc0a5e3a4dd1fcce0547e87df
Raphael-Alves-S <rapha.alves75@gmail.com>
2020-03-16 14:03:53 -0300

    teste

--- src/app/endereco-novo/endereco-novo.component.ts
+++ src/app/endereco-novo/endereco-novo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-endereco-novo',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./endereco-novo.component.scss']
 })
 export class EnderecoNovoComponent implements OnInit {
-
-  constructor() { }
+  enderecoForm: FormGroup;
+  isLoadingResults = false;
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.enderecoForm =  this.formBuilder.group({
+      'logradouro': [null, [Validators.required]],
+      'bairro': [null, [Validators.required]],
+      'cidade': [null, [Validators.required]],
+      'cep': [null, [Validators.required]],
+      'uf': [null,[Validators.required]]
+    });
+  }
+
+  addEndereco(form: NgForm) {
+    this.isLoadingResults =  true;
+    this.api.addEndereco(form)
+    .subscribe(res => {
+      // tslint:disable-next-line: no-string-literal
+      const id = res['id'];
+      this.isLoadingResults =  false;
+      this.router.navigate(['/endereco-detalhe', id]);
+    },
+    (err) => {
+      console.log(err);
+      this.isLoadingResults = false;
+    });
   }
 
 }
